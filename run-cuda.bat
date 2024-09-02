57918b1cf2aef01a723bb36a3d989a279a98e79c
Firmin.sun <fmsunyh@gmail.com>
2024-09-02 15:10:22 +0800

    bug fix

--- run-cuda.bat
+++ run-cuda.bat
@@ -6,6 +6,7 @@ python run.py --execution-provider cuda --execution-threads 60 --max-memory 60 -
 python run.py --execution-provider cuda --execution-threads 60 --max-memory 60 --source "demo/images/hgf.jpg" --rtmp_output "rtmp://183.232.228.244:1935/live"
 python run.py --execution-provider cuda --execution-threads 60 --max-memory 60 --source "demo/images/hgf.jpg" --rtmp_input "rtmp://183.232.228.244:1935/live_input" --rtmp_output "rtmp://183.232.228.244:1935/live1"
 
+D:\code\FaceFusionLive\Dlab\python run.py --execution-provider cuda --execution-threads 60 --max-memory 60 --source "demo/images/hgf.jpg" --rtmp_input "rtmp://183.232.228.244:1935/live_input" --rtmp_output "rtmp://183.232.228.244:1935/live1"
 
 --source "demo/images/masik.PNG"
 --source "demo/images/hgf.jpg"
